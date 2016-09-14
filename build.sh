15e23f7df3030d8d90f6659a052ae12113aee15c
Nidin Vinayakan <01@01alchemist.com>
2016-09-14 16:00:55 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+
+mkdir js_build
+cd js_build
+cmake .. -G "Unix Makefiles" -DEMBREE_ISPC_SUPPORT=OFF
+emmake make
+
+emcc libembree.so libembree.so.2 libembree.so.2.11.0 \
+libimage.a liblexers.a liblights.a libnoise.a libscenegraph.a \
+libsimd.a libsys.a libtexture.a libtutorial.a libtutorial_device.a \
+libembree_avx.a libembree_avx2.a libembree_sse42.a \
+-o embree.js -s TOTAL_MEMORY=167772160 -s USE_PTHREADS=1 -s SIMD=1
